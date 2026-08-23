ff803e037d0b25f3d341d92b0215e13037521820
Costa-dias <xadoondias@gmail.com>
2026-08-23 17:06:41 -0300

    Add PublicSite component with layout structure

--- src/components/PublicSite.tsx
+++ src/components/PublicSite.tsx
@@ -1,1 +1,22 @@
+import { Header } from './Header';
+import { Hero } from './Hero';
+import { Catalog } from './Catalog';
+import { Footer } from './Footer';
+import { SocialFloat } from './SocialFloat';
+
+type Props = {
+  onAdminClick: () => void;
+};
+
+export function PublicSite({ onAdminClick }: Props) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header onAdminClick={onAdminClick} />
+      <Hero />
+      <Catalog />
+      <Footer />
+      <SocialFloat />
+    </div>
+  );
+}
 
